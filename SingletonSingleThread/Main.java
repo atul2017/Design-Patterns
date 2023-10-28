46499f084aec204199cee4d31f56711d3db610e5
ATUL PATEL <connecttoatul@gmail.com>
2023-10-28 13:57:36 -0500

    Create Main.java

---
+++ SingletonSingleThread/Main.java
@@ -0,0 +1,11 @@
+package SingletonSingleThread;
+
+public class Main {
+    public static void main(String[] args){
+        Singleton singleton = Singleton.getSingleton();
+        Singleton singleton1 = Singleton.getSingleton();
+        if(singleton==singleton1){
+            System.out.println("Singleton & singleton1 are same objects");
+        }
+    }
+}
