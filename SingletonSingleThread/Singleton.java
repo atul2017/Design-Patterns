9899e9e453f26994f863cbad87e1e382f36721d6
ATUL PATEL <connecttoatul@gmail.com>
2023-10-28 13:57:33 -0500

    Create Singleton.java

---
+++ SingletonSingleThread/Singleton.java
@@ -0,0 +1,17 @@
+package SingletonSingleThread;
+
+public class Singleton {
+    //eagerly loading
+    //private static Singleton singleton = new Singleton();
+
+    //Lazily loading
+    private static Singleton singleton = null;
+
+    private Singleton(){}
+    public static Singleton getSingleton(){
+        if(singleton==null){
+            singleton = new Singleton();
+        }
+      return singleton;
+    }
+}
