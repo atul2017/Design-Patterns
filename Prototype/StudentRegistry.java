4e7b16462d9be06d71bf008c34e49a29b30b5b4f
ATUL PATEL <connecttoatul@gmail.com>
2023-10-31 21:14:08 -0500

    Create StudentRegistry.java

---
+++ Prototype/StudentRegistry.java
@@ -0,0 +1,15 @@
+package Prototype;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class StudentRegistry {
+    private Map<String,Student> map = new HashMap<>();
+
+    public void register(String st,Student student){
+        map.put(st,student);
+    }
+    Student get(String key){
+        return map.get(key);
+    }
+}
