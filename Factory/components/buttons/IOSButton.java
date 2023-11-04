dc3944b61c768bda171cfd3571d5e68a662de374
ATUL PATEL <connecttoatul@gmail.com>
2023-11-03 22:46:39 -0500

    Create IOSButton.java

---
+++ Factory/components/buttons/IOSButton.java
@@ -0,0 +1,11 @@
+package Factory.components.buttons;
+
+public class IOSButton implements Button{
+
+
+    @Override
+    public void changeSize() {
+        System.out.println("Change the size of the IOS button");
+
+    }
+}
