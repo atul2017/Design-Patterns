4e47790092b1a845fef54536aa74541fa9aed543
ATUL PATEL <connecttoatul@gmail.com>
2023-11-03 22:46:34 -0500

    Create AndriodButton.java

---
+++ Factory/components/buttons/AndriodButton.java
@@ -0,0 +1,9 @@
+package Factory.components.buttons;
+
+public class AndriodButton implements Button {
+
+    @Override
+    public void changeSize() {
+        System.out.println("Change the size of the andriod button");
+    }
+}
